86956d330eef3a310c58d82f34e356cfbb765435
Nikita <predatorrr@mail.ru>
2024-08-22 11:36:18 +0500

    Lazy Column

--- app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainViewModel.kt
@@ -1,7 +1,7 @@
 package com.sumin.vknewsclient
 
 import androidx.lifecycle.ViewModel
-import com.sumin.vknewsclient.domain.FeedPost
+import com.sumin.vknewsclient.domain.FeedPostModel
 import com.sumin.vknewsclient.domain.StatisticItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,12 +9,19 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
 
-    private val _feedPost: MutableStateFlow<FeedPost> = MutableStateFlow(FeedPost())
-    val feedPost: StateFlow<FeedPost> = _feedPost.asStateFlow()
+    private val newsList = mutableListOf<FeedPostModel>().apply {
+        repeat(10) {
+            add(FeedPostModel(id = it))
+        }
+    }
 
-    fun updateCount(item: StatisticItem) {
-        val oldStatistic = feedPost.value.statistics
+    private val _feedPosts: MutableStateFlow<List<FeedPostModel>> =
+        MutableStateFlow(newsList)
+    val feedPosts: StateFlow<List<FeedPostModel>> = _feedPosts.asStateFlow()
 
+    fun updateCount(feedPostModel: FeedPostModel, item: StatisticItem) {
+        val oldPost = _feedPosts.value.toMutableList()
+        val oldStatistic = feedPostModel.statistics
         val newStatistic = oldStatistic.toMutableList().apply {
             replaceAll { oldItem ->
                 if (oldItem.type == item.type) {
@@ -24,6 +31,21 @@ class MainViewModel : ViewModel() {
                 }
             }
         }
-        _feedPost.value = feedPost.value.copy(statistics = newStatistic)
+        val newFeedPost = feedPostModel.copy(statistics = newStatistic)
+        _feedPosts.value = oldPost.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) {
+                    newFeedPost
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    fun deletePost(model: FeedPostModel) {
+        val modifiedList = _feedPosts.value.toMutableList()
+        modifiedList.remove(model)
+        _feedPosts.value = modifiedList
     }
 }
--- app/src/main/java/com/sumin/vknewsclient/domain/FeedPostModel.kt
+++ app/src/main/java/com/sumin/vknewsclient/domain/FeedPostModel.kt
@@ -2,7 +2,8 @@ package com.sumin.vknewsclient.domain
 
 import com.sumin.vknewsclient.R
 
-data class FeedPost(
+data class FeedPostModel(
+    val id: Int = 0,
     val communityName: String = "/dev/null",
     val publicationDate: String = "14:00",
     val avatarResId: Int = R.drawable.post_comunity_thumbnail,
