f67771fa36788a80496cbddedf7334356446889b
Nikita <predatorrr@mail.ru>
2024-08-23 23:00:11 +0500

    Refactoring navigation

---
+++ app/src/main/java/com/sumin/vknewsclient/presentation/navigation/NavigationState.kt
@@ -0,0 +1,29 @@
+package com.sumin.vknewsclient.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+class NavigationState(val navHostController: NavHostController) {
+
+    fun navigateTo(route: String) {
+        navHostController.navigate(route) {
+            popUpTo(navHostController.graph.startDestinationId) {
+                saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
+}
+
+
+@Composable
+fun rememberNavState(
+    navHostController: NavHostController = rememberNavController()
+): NavigationState {
+    return remember {
+        NavigationState(navHostController)
+    }
+}
